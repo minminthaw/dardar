7f731d1ec11b689cc44f14318229509f110f681a
dardarms <73411697+dardarms@users.noreply.github.com>
2020-11-21 03:27:05 +0630

    Update facebook thitnaingaye.c

--- facebook thitnaingaye.c
+++ facebook thitnaingaye.c
@@ -1173,8 +1173,6 @@ https://www.livedoomovies.com/02_epl2_720p/chunklist.m3u8
 https://www.livedoomovies.com/02_epl1_720p_UK/chunklist.m3u8
 #EXTINF:-1 tvg-logo="https://i.pinimg.com/736x/9f/f9/bb/9ff9bba60c3b1a182a338e9868a9117a.jpg" group-title="BeIN Sports",BeIN SPORT 2 ok
 https://www.livedoomovies.com/02_epl2_720p_UK/chunklist.m3u8
-#PLAYLIST:Music TV
-https://short.absidev.com/mymmym
 
 
 
